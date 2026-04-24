0ea7a8eb78b978c4f4ee2e6f2c1b101737d74a89
JolieBao <@>
2026-04-24 12:15:55 -0400

    Base UI is done!

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function switchTabs() {
+    const tabs = document.querySelectorAll("#UIBar .tab");
+    const panels = document.querySelectorAll(".panel");
 
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+        tabs.forEach(t => t.classList.remove("active"));
+        panels.forEach(p => p.style.display = "none");
+
+        tab.classList.add("active");
+
+        const target = tab.dataset.tab;
+        document.getElementById(target).style.display = "block";
+        });
+    });
+}
